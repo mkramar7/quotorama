b52f2302cd28f854ddb68337706946ff487d5804
Marko Kramar <marko.kramar7@gmail.com>
2020-12-27 19:38:22 +0100

    Initial Commit

---
+++ Quotorama/QuotoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotoramaApp.swift
+//  Quotorama
+//
+//  Created by Marko Kramar on 27.12.2020..
+//
+
+import SwiftUI
+
+@main
+struct QuotoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
