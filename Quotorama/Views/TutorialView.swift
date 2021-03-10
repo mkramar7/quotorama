3a6f43e7d1365beb03f762cfa904526cfa4665a2
Marko Kramar <marko.kramar7@gmail.com>
2021-03-10 15:32:41 +0100

    Make improvements on tutorial player

--- Quotorama/Views/TutorialView.swift
+++ Quotorama/Views/TutorialView.swift
@@ -11,44 +11,49 @@ import AVKit
 struct TutorialView: View {
     @Binding var viewShown: Bool
     
+    let player = AVPlayer(url:  Bundle.main.url(forResource: "swipe_left", withExtension: "mp4")!)
+    
     var body: some View {
         VStack {
             Text("Swipe left to show next quote")
+                .padding(.top, 10)
             
-            AVTutorialPlayerView()
+            AVTutorialPlayerView(player: player)
+                .frame(width: 125, height: 125)
             
             Divider()
                 .background(Color.white)
-                .padding(.bottom, 10)
+            
             Button("OK") {
                 self.viewShown = false
             }
+            .foregroundColor(.accentColor)
+            .padding(.bottom, 10)
         }
-        .frame(width: 250, height: 200)
-        .padding(20)
-        .background(Color(UIColor.darkGray))
-        .foregroundColor(.white)
+        .frame(width: 250)
+        .foregroundColor(.black)
+        .background(Color.white)
         .cornerRadius(10)
     }
 }
 
 struct AVTutorialPlayerView: UIViewControllerRepresentable {
-    private var player: AVPlayer {
-        AVPlayer(url:  Bundle.main.url(forResource: "swipe_left", withExtension: "mp4")!)
-    }
+    var player: AVPlayer
     
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         let playerController = uiViewController as! AVPlayerViewController
+        playerController.view.backgroundColor = UIColor.white
         playerController.player = player
         playerController.player?.play()
         playerController.showsPlaybackControls = false
-        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: self.player.currentItem, queue: .main) { _ in
-            player.seek(to: CMTime.zero)
-            player.play()
-        }
+        
     }
     
     func makeUIViewController(context: Context) -> some UIViewController {
+        NotificationCenter.default.addObserver(forName: .AVPlayerItemDidPlayToEndTime, object: player.currentItem, queue: .main) { _ in
+            player.seek(to: CMTime.zero)
+            player.play()
+        }
         return AVPlayerViewController()
     }
 }
